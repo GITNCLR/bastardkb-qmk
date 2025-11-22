288e45cdb53261a4f5e063edef3c6b14c9a1ce55
Noah Christian Le Roy <noahleroy@gmail.com>
2025-11-22 18:55:31 +0100

    dragscroll mode automouse rollback

--- keyboards/bastardkb/charybdis/charybdis.c
+++ keyboards/bastardkb/charybdis/charybdis.c
@@ -393,11 +393,6 @@ bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
             break;
         case DRAGSCROLL_MODE:
             charybdis_set_pointer_dragscroll_enabled(record->event.pressed);
-#            ifdef POINTING_DEVICE_AUTO_MOUSE_ENABLE
-            if (record->event.pressed && !get_auto_mouse_toggle()) {
-                auto_mouse_toggle(); // pressing the mode re-enables auto mouse
-            }
-#            endif
             break;
         case DRAGSCROLL_MODE_TOGGLE:
             if (record->event.pressed) {
